e2f57ae49c13397d04db3009377996a1f6d4ffc1
Brad Mallow <briznad@gmail.com>
2025-03-24 13:29:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ionic-modal-footer-test
+
+Test ion-footer within ion-modal
+
+## steps to reproduce
+
+### 1. run test app
+
+Clone or download this repo to your local machine, then navigate to the project directory in your terminal of choice.
+
+```bash
+npm run dev -- --open
+```
+
+### 2. in test app click Open button
+
+### 3. observe modal
+
+## Expected
+
+### modal should display with properly-rendered header & footer
+
+## Actual
+
+### I'm seeing the expected result
+
+<img width="1056" alt="Screenshot 2025-03-24 at 12 34 53 PM" src="https://github.com/user-attachments/assets/e968e62d-4855-4122-b5ae-fb11b09f2ac4" />
+
+<img width="612" alt="Screenshot 2025-03-24 at 12 35 10 PM" src="https://github.com/user-attachments/assets/f77e1c80-5bcd-4161-9389-d3eb8a6e38fe" />
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,74 @@
+<script lang="ts" module>
+  import { setupIonicBase } from "@ionic-sveltekit/core";
+
+  import "@ionic-sveltekit/core/components/all";
+
+  /*
+		This part - import '@ionic-sveltekit/core/components/all'; -  loads all components at once.
+
+		This adds at least >800kb (uncompressed) to your bundle - 80 components (so do your math!!)
+
+		You can also choose to import each component you want to use separately and leave out others.
+
+		It is recommended to do this in this file, as you only need to do such once. But you are free
+		to do this elsewhere if you like to code-split differently.
+
+		Example: if you replace the line import '@ionic-sveltekit/core/components/all'; with the imports below, you will see the resulting bundle being much smaller
+
+		import '@ionic-sveltekit/core/components/ion-app';
+		import '@ionic-sveltekit/core/components/ion-card';
+		import '@ionic-sveltekit/core/components/ion-card-title';
+		import '@ionic-sveltekit/core/components/ion-card-subtitle';
+		import '@ionic-sveltekit/core/components/ion-card-header';
+		import '@ionic-sveltekit/core/components/ion-card-content';
+		import '@ionic-sveltekit/core/components/ion-chip';
+		import '@ionic-sveltekit/core/components/ion-button';
+
+		Open @ionic-sveltekit/core/components/all to view the full list of possible imports
+	*/
+
+  /* Global CSS */
+  import "../theme/global.css";
+
+  /* Theme variables */
+  import "../theme/variables.css";
+</script>
+
+<script lang="ts">
+  /* Call Ionic's setup routine */
+  setupIonicBase({
+    mode: "ios",
+  });
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Ionic SvelteKit" />
+  <link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
+
+<ion-app>
+  <ion-content>
+    <main>
+      {@render children()}
+    </main>
+  </ion-content>
+</ion-app>
+
+<style>
+  main {
+    max-width: 100vw;
+    justify-self: center;
+  }
+
+  @media (min-width: 600px) {
+    main {
+      max-width: 580px;
+    }
+  }
+</style>
